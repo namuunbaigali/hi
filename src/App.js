bed2f84ed7cd932c35d3e9b668262fb915e9738d
Намуунбайгаль <118256485+namuunbaigali@users.noreply.github.com>
2024-01-07 21:37:59 +0800

    as

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Hero from "./components/Hero";
+// import Navbar from "./Layouts/Navbar";
+// import Skills from "./components/Skills";
+// import Service from "./components/Services";
+// import Projects from "./components/Projects";
+// import Testimonials from "./components/Testimonials";
+import Hireme from "./components/Hireme";
+import Contact from "./components/Contact";
+import Aos from "aos";
+import "aos/dist/aos.css";
+// import Login from "./components/Login";
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    Aos.init({
+      duration: 1800,
+      offset: 100,
+      disable: "mobile",
+    });
+  }, []);
+
+  const handleLogin = () => {
+    // Perform your login logic here
+    // For simplicity, we'll toggle the login state
+    setIsLoggedIn(!isLoggedIn);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="italic">
+      {isLoggedIn ? (
+        <>
+          {/* <Navbar /> */}
+          <Hero />
+          {/* <Service /> */}
+          {/* <Skills /> */}
+          {/* <Projects /> */}
+          {/* <Testimonials /> */}
+          {/* <MyPhotoAlbum /> */}
+          <Hireme />
+          <Contact />
+
+          {/* <button className="border border-[red] bg-blue-600 p-8">
+            hi
+          </button> */}
+          <footer className="p-3 text-center not-italic">
+            <h6 className="mb-3">GARID & NAMUUK</h6>
+            <p>Эцсийн хүчээ шавхаад энэ амьдралд чамайг л авч үлдмээр байна.</p>
+            <h2> ХАМГИЙН САЙХАН НЬ . . .</h2>
+          </footer>
+        </>
+      ) : (null
+        // <Login onLogin={handleLogin} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
